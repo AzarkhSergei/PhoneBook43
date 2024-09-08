af586c1f1fa17abcf6c49905e4ac71f076c0f3c2
AzarkhSergei <sergei.azarkh@gmail.com>
2024-09-08 19:34:32 +0300

    added dataprovider2

--- src/test/java/restassured/RegistrationTest.java
+++ src/test/java/restassured/RegistrationTest.java
@@ -31,24 +31,27 @@ public class RegistrationTest implements TestHelper {
   @DataProvider(name = "RegistrationData")
   public Object[][] RegistrationData() {
     return new Object[][]{
-        {RANDOM_EMAIL, PASSWORD_WITHOUT_DIGITS},
-        {RANDOM_EMAIL, PASSWORD_WITH_LENGTH_LESS_8_SYMBOLS},
-        {RANDOM_EMAIL, PASSWORD_WITHOUT_LOWERCASE_LETTER},
-        {RANDOM_EMAIL, PASSWORD_WITHOUT_SPECIAL_SYMBOL},
-        {RANDOM_EMAIL, PASSWORD_WITHOUT_UPPERCASE_LETTER},
-        //{RANDOM_EMAIL, PASSWORD_WITH_LENGTH_MORE_15_SYMBOLS},
-        {RANDOM_EMAIL_WITHOUT_USER_NAME, RANDOM_PASSWORD},
-        {RANDOM_EMAIL_WITHOUT_DOMAIN, RANDOM_PASSWORD},
-        {RANDOM_EMAIL_WITH_INVALID_CHAR, RANDOM_PASSWORD},
-        {RANDOM_EMAIL_WITHOUT_AT, RANDOM_PASSWORD},
-        //{RANDOM_EMAIL_WITHOUT_DOT, RANDOM_PASSWORD},
-        {"", RANDOM_PASSWORD},
-        {RANDOM_EMAIL, ""},
+        {RANDOM_EMAIL, PASSWORD_WITHOUT_DIGITS, 400},
+        {RANDOM_EMAIL, PASSWORD_WITH_LENGTH_LESS_8_SYMBOLS, 400},
+        {RANDOM_EMAIL, PASSWORD_WITHOUT_LOWERCASE_LETTER, 400},
+        {RANDOM_EMAIL, PASSWORD_WITHOUT_SPECIAL_SYMBOL, 400},
+        {RANDOM_EMAIL, PASSWORD_WITHOUT_UPPERCASE_LETTER, 400},
+        //{RANDOM_EMAIL, PASSWORD_WITH_LENGTH_MORE_15_SYMBOLS, 400},
+        {RANDOM_EMAIL_WITHOUT_USER_NAME, RANDOM_PASSWORD, 400},
+        {RANDOM_EMAIL_WITHOUT_DOMAIN, RANDOM_PASSWORD, 400},
+        {RANDOM_EMAIL_WITH_INVALID_CHAR, RANDOM_PASSWORD, 400},
+        {RANDOM_EMAIL_WITHOUT_AT, RANDOM_PASSWORD, 400},
+        //{RANDOM_EMAIL_WITHOUT_DOT, RANDOM_PASSWORD, 400},
+        {"", RANDOM_PASSWORD, 400},
+        {RANDOM_EMAIL, "", 400},
+        {RANDOM_EMAIL, null, 400},
+        {null, RANDOM_PASSWORD, 400},
+        {PropertiesReaderXML.getProperties(CORRECT_EMAIL, XML_DATA_FILE), RANDOM_PASSWORD, 409},
     };
   }
 
   @Test(dataProvider = "RegistrationData")
-  public void registrationNegativeTests(String email, String password){
+  public void registrationNegativeTests(String email, String password, int expectedStatusCode){
     AuthenticationRequestModel requestModel = AuthenticationRequestModel.builder()
         .username(email)
         .password(password)
@@ -58,29 +61,10 @@ public class RegistrationTest implements TestHelper {
         .contentType(ContentType.JSON)
         .when()
         .post(BASE_URL+REGISTRATION_PATH)
-        .then().assertThat().statusCode(400)
+        .then().assertThat().statusCode(expectedStatusCode)
         .extract().as(ErrorModel.class);
     System.out.println(errorModel.getMessage());
     System.out.println(errorModel.getStatus());
   }
-
-  @Test
-  public void registrationDuplicateUser(){
-    AuthenticationRequestModel authenticationRequestModel = AuthenticationRequestModel
-        .builder()
-        .username(PropertiesReaderXML.getProperties(CORRECT_EMAIL, XML_DATA_FILE))
-        .password(RANDOM_PASSWORD)
-        .build();
-    ErrorModel errorModel = given()
-        .body(authenticationRequestModel)
-        .contentType(ContentType.JSON)
-        .when()
-        .post(BASE_URL+REGISTRATION_PATH)
-        .then().assertThat().statusCode(409)
-        .extract().as(ErrorModel.class);
-    System.out.println(errorModel.getMessage());
-    System.out.println(errorModel.getStatus());
-  }
-
 
 }
